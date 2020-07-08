5bef71e56f795d86f476d8f8e217339ecb00596e
Asha Ramanjanaiah <asha.ramanjanaiah@gmail.com>
2020-07-08 17:25:20 +1000

    Created tab layout with 3 tabs and fragments.

---
+++ app/src/main/java/com/weather/articlesheadlinesapp/ui/main/HeadlinesFragment.kt
@@ -0,0 +1,58 @@
+package com.weather.articlesheadlinesapp.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.weather.articlesheadlinesapp.R
+
+/**
+ * A Headlines fragment containing headlines of articles.
+ */
+class HeadlinesFragment : Fragment() {
+
+    private lateinit var pageViewModel: PageViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
+            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_main, container, false)
+        val textView: TextView = root.findViewById(R.id.section_label)
+        pageViewModel.text.observe(this, Observer<String> {
+            textView.text = it
+        })
+        return root
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private const val ARG_SECTION_NUMBER = "section_number"
+
+        /**
+         * Returns a new instance of this fragment.
+         */
+        @JvmStatic
+        fun newInstance(sectionNumber: Int): HeadlinesFragment {
+            return HeadlinesFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_SECTION_NUMBER, sectionNumber)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/weather/articlesheadlinesapp/ui/main/SavedFragment.kt
@@ -0,0 +1,59 @@
+package com.weather.articlesheadlinesapp.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.weather.articlesheadlinesapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SavedFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SavedFragment : Fragment() {
+    private lateinit var pageViewModel: PageViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
+            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_main, container, false)
+        val textView: TextView = root.findViewById(R.id.section_label)
+        pageViewModel.text.observe(this, Observer<String> {
+            textView.text = it
+        })
+        return root
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private const val ARG_SECTION_NUMBER = "section_number"
+
+        /**
+         * Returns a new instance of this fragment.
+         */
+        @JvmStatic
+        fun newInstance(sectionNumber: Int): SavedFragment {
+            return SavedFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_SECTION_NUMBER, sectionNumber)
+                }
+            }
+        }
+    }
+}
